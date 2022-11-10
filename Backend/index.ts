4989f5c68d0d57558e99a1d080307b434916a4a2
Krunal Bhandekar <krunalbhandekar10@gmail.com>
2022-11-10 16:21:46 +0530

    initial

---
+++ Backend/index.ts
@@ -0,0 +1,24 @@
+const express=require("express");
+require("dotenv").config();
+
+const app=express();
+app.use(express.json());
+
+const connection=require("./config")as number;
+
+app.get("/",(req,res)=>{
+    return res.status(200).send("HomePage")
+})
+
+app.listen(process.env.PORT,async()=>{
+    try{
+        await connection
+        console.log("DB Connected")
+    }
+    catch(err){
+        console.log(err)
+
+    }
+    console.log(`DB Connected at port ${process.env.PORT}`)
+})
+
